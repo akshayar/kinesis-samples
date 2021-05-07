64fe72eba4186ea34f8fcc5450a4628b62b00405
rawaaksh+1-at-799223504601 <7/C7RScx94kBHBQuoILFIoAfEHeS7CHJNVo15CyGduE=>
2021-05-07 20:53:21 +0530

    First Draft

--- kinesis-consumers/kcl-consumer/run-task.sh
+++ kinesis-consumers/kcl-consumer/run-task.sh
@@ -1,5 +1,5 @@
 aws ecs run-task \
         --cluster fargate-cluster \
         --task-definition kcl-consumer \
-        --launch-type FARGATE 
-        --network-configuration "awsvpcConfiguration={subnets=['subnet-0407b1173827ed96a','subnet-12345'],securityGroups=['test'],assignPublicIp='DISABLED'}"
+        --launch-type FARGATE \
+        --network-configuration "awsvpcConfiguration={subnets=['subnet-0407b1173827ed96a'],securityGroups=['sg-05b84f422b01057df'],assignPublicIp='DISABLED'}"
